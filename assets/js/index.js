860e901794ecdee1d65164dd878b57ac59c3e571
betson <benjamin.getson@gsa.gov>
2016-09-12 15:10:07 -0400

    format product listings to use ellipsis structure

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,25 @@
 (function (document, window, $) {
   $(document).ready(function(){
-    ajaxifyContactForm();
+    updateProductListings();
+    //ajaxifyContactForm();
   });
+
+  function updateProductListings() {
+    var listings = $('#product-listings>ul');
+    
+    // Create <div><div><p> structure required for ellipsis solution
+    // http://www.mobify.com/blog/multiline-ellipsis-in-pure-css/
+    var ellipsisDiv = document.createElement('div');
+    ellipsisDiv.className = "ellipsis";
+    var innerDiv = document.createElement('div');
+    ellipsisDiv.appendChild(innerDiv);
+
+    listings.children().each(function() {
+      var ellipsisCopy = ellipsisDiv.cloneNode(true);
+      $(this).children('p').detach().appendTo(ellipsisCopy.children[0]);
+      $(this).append(ellipsisCopy);
+    });
+  }
 
   function ajaxifyContactForm () {
     var contactForm = $('#contact'),
